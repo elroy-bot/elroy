947f1b5de3c12a0ad9414e01ec86132b35765ee5
middle_aged_coder <noreply@elroy.bot>
2024-10-06 12:32:48 -0700

    Restructure cli app

--- elroy/config.py
+++ elroy/config.py
@@ -1,14 +1,14 @@
 import os
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
-from dotenv import load_dotenv
+import configargparse
 from sqlalchemy import Engine, NullPool, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlmodel import Session
 from toolz import assoc, pipe
-from toolz.curried import merge, valfilter
+from toolz.curried import valfilter
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -16,7 +16,6 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class ElroyEnv(Enum):
     TESTING = "testing"
     LOCAL = "local"
-    DOCKER_LOCAL = "docker_local"
 
 
 @dataclass
@@ -37,35 +36,24 @@ def str_to_bool(input: Optional[str]) -> bool:
 
 
 def get_config() -> ElroyConfig:
-
-    env = _get_elroy_env()
-
-    if env == ElroyEnv.TESTING:
-        params = _load_testing_config()
-    elif env == ElroyEnv.LOCAL:
-        params = _load_local_config()
-    elif env == ElroyEnv.DOCKER_LOCAL:
-        params = _load_docker_local()
-    else:
-        raise ValueError(f"Unknown environment: {env}")
-
-    # merge env specific vars, priority to env vars
-    env_variables = valfilter(
-        lambda x: x is not None,
-        {
-            "database_url": os.environ.get("ELROY_DATABASE_URL"),
-            "context_window_token_limit": (
-                int(os.environ["ELROY_CONTEXT_WINDOW_TOKEN_LIMIT"]) if os.environ.get("ELROY_CONTEXT_WINDOW_TOKEN_LIMIT") else None
-            ),
-            "openai_api_key": os.environ.get("OPENAI_API_KEY"),
-            "local_storage_path": os.environ.get("ELROY_LOCAL_STORAGE_PATH"),
-        },
+    parser = configargparse.ArgParser(default_config_files=["/etc/elroy/config.ini", "~/.elroy.ini"])
+    parser.add_argument("--config", is_config_file=True, help="config file path")
+    parser.add_argument("--database_url", env_var="ELROY_DATABASE_URL", required=True, help="Database URL")
+    parser.add_argument("--openai_api_key", env_var="OPENAI_API_KEY", required=True, help="OpenAI API Key")
+    parser.add_argument("--local_storage_path", env_var="ELROY_LOCAL_STORAGE_PATH", default=".cache", help="Local storage path")
+    parser.add_argument(
+        "--context_window_token_limit",
+        env_var="ELROY_CONTEXT_WINDOW_TOKEN_LIMIT",
+        type=int,
+        default=16384,
+        help="Context window token limit",
     )
 
+    args = parser.parse_args()
+
     config = pipe(
-        params,
+        vars(args),
         valfilter(lambda x: x is not None),
-        lambda x: merge(x, env_variables),  # env specific configs are priority
         lambda x: assoc(x, "engine", create_engine(x["database_url"], poolclass=NullPool)),
         lambda x: assoc(x, "declarative_base", declarative_base()),
         lambda x: assoc(x, "session_maker", sessionmaker(bind=x["engine"])),
@@ -76,29 +64,6 @@ def get_config() -> ElroyConfig:
     assert isinstance(config, ElroyConfig)
 
     return config
-
-
-def _load_testing_config() -> Dict:
-    load_dotenv(dotenv_path=os.path.join(ROOT_DIR, "tests", ".env"), override=True)
-    return {
-        "local_storage_path": os.path.join(os.getcwd(), ".cache", "test"),
-        "context_window_token_limit": 1000,
-    }
-
-
-def _load_local_config() -> Dict:
-    load_dotenv(dotenv_path=os.path.join(ROOT_DIR, ".env"), override=True)
-    return {
-        "local_storage_path": os.getenv("LOCAL_STORAGE_PATH", ".cache"),
-        "context_window_token_limit": 16384,
-    }
-
-
-def _load_docker_local() -> Dict:
-    return {
-        "local_storage_path": os.getenv("LOCAL_STORAGE_PATH", ".cache"),
-        "context_window_token_limit": 16384,
-    }
 
 
 from contextlib import contextmanager
@@ -121,8 +86,6 @@ def session_manager() -> Generator[Session, None, None]:
 def _get_elroy_env() -> ElroyEnv:
     if os.environ.get("PYTEST_VERSION"):
         return ElroyEnv.TESTING
-    elif os.environ.get("DOCKER_LOCAL"):
-        return ElroyEnv.DOCKER_LOCAL
     else:
         return ElroyEnv.LOCAL
 
