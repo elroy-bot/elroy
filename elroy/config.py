50f5a53ef74c1821ff8824c1de8d3b49e0ce2568
middle_aged_coder <noreply@elroy.bot>
2024-10-17 15:33:34 -0700

    Add option for docker db to start

--- elroy/config.py
+++ elroy/config.py
@@ -7,8 +7,6 @@ from rich.console import Console
 from sqlalchemy import NullPool, create_engine
 from sqlmodel import Session
 
-from elroy.logging_config import setup_logging
-
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -25,7 +23,6 @@ class ElroyConfig:
     context_window_token_limit: int
     context_refresh_token_trigger_limit: int  # how many tokens we reach before triggering refresh
     context_refresh_token_target: int  # how many tokens we aim to have after refresh
-    log_file_path: str
 
 
 def str_to_bool(input: Optional[str]) -> bool:
@@ -37,13 +34,8 @@ def get_config(
     openai_api_key: str,
     local_storage_path: Optional[str] = None,
     context_window_token_limit: Optional[int] = None,
-    log_file_path: Optional[str] = None,
 ) -> ElroyConfig:
-    log_file_path = log_file_path or os.path.join(ROOT_DIR, "logs", "elroy.log")
     context_window_token_limit = context_window_token_limit or 16384
-
-    # Set up logging
-    setup_logging(log_file_path)
 
     return ElroyConfig(
         database_url=database_url,
@@ -52,7 +44,6 @@ def get_config(
         context_window_token_limit=context_window_token_limit,
         context_refresh_token_trigger_limit=int(context_window_token_limit * 0.66),
         context_refresh_token_target=int(context_window_token_limit * 0.33),
-        log_file_path=log_file_path,
     )
 
 
