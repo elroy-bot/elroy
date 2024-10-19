4ed10e06a970edda27cce2cfff04456b12360bc9
middle_aged_coder <noreply@elroy.bot>
2024-10-18 20:32:39 -0700

    feat: Add rename_goal function

--- elroy/store/store.py
+++ elroy/store/store.py
@@ -10,7 +10,8 @@ def get_archival_messages(context: ElroyContext) -> Iterable[ArchivalMemory]:
     return context.session.exec(select(ArchivalMemory).where(ArchivalMemory.user_id == context.user_id)).all()
 
 
-def persist_archival_memory(context: ElroyContext, name: str, text: str) -> None:
+# Need to check existing goals for similarity
+def create_archival_memory(context: ElroyContext, name: str, text: str) -> None:
     archival_memory = ArchivalMemory(user_id=context.user_id, name=name, text=text)
     context.session.add(archival_memory)
     context.session.commit()
